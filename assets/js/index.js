56a97a92aee8c25b3eab9eea95aad96b0f425e86
whiteWolfx99 <hhqu180099@uniq.edu.iq>
2022-10-11 01:42:58 +0300

    driver: hevar; Navigator: Luis

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,36 +1,30 @@
-//const navbarOpen = document.getElementById('nabvar-h');
+// const navbarOpen = document.getElementById('nabvar-h');
 const navBarOpen = document.querySelector('#navbar-h');
-navBarOpen.addEventListener('click', function () {
-    const navbarOptions = document.querySelector('#navbar-options');
-    navbarOptions.classList.replace('nav-right-text', 'nav-right-text-open');
-}
-);
+navBarOpen.addEventListener('click', () => {
+  const navbarOptions = document.querySelector('#navbar-options');
+  navbarOptions.classList.replace('nav-right-text', 'nav-right-text-open');
+});
 
 const navBarClose = document.querySelector('#navbar-c');
-navBarClose.addEventListener('click', function () {
-    const navbarOptions = document.querySelector('#navbar-options');
-    navbarOptions.classList.replace('nav-right-text-open', 'nav-right-text');
-}
-);
+navBarClose.addEventListener('click', () => {
+  const navbarOptions = document.querySelector('#navbar-options');
+  navbarOptions.classList.replace('nav-right-text-open', 'nav-right-text');
+});
 
 const sectionWorkClose = document.querySelector('#work-c');
-sectionWorkClose.addEventListener('click', function () {
-    const navbarOptions = document.querySelector('#navbar-options');
-    navbarOptions.classList.replace('nav-right-text-open', 'nav-right-text');
-}
-);
+sectionWorkClose.addEventListener('click', () => {
+  const navbarOptions = document.querySelector('#navbar-options');
+  navbarOptions.classList.replace('nav-right-text-open', 'nav-right-text');
+});
 
 const sectionAboutClose = document.querySelector('#about-c');
-sectionAboutClose.addEventListener('click', function () {
-    const navbarOptions = document.querySelector('#navbar-options');
-    navbarOptions.classList.replace('nav-right-text-open', 'nav-right-text');
-}
-);
+sectionAboutClose.addEventListener('click', () => {
+  const navbarOptions = document.querySelector('#navbar-options');
+  navbarOptions.classList.replace('nav-right-text-open', 'nav-right-text');
+});
 
 const sectionContactClose = document.querySelector('#contact-c');
-sectionContactClose.addEventListener('click', function () {
-    const navbarOptions = document.querySelector('#navbar-options');
-    navbarOptions.classList.replace('nav-right-text-open', 'nav-right-text');
-}
-);
-
+sectionContactClose.addEventListener('click', () => {
+  const navbarOptions = document.querySelector('#navbar-options');
+  navbarOptions.classList.replace('nav-right-text-open', 'nav-right-text');
+});
