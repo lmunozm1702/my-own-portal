f9eb210c1e6a922e9a9064350e4f76114094508a
Luis Muñoz <lmunozm1702@gmail.com>
2022-10-10 21:50:33 -0300

    Add styles and and close action to navbar
    Update style.css and index.html to apply styles to the mobile navbar.
    
    Update index.js to implement close action to the navbar.

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,12 +3,18 @@ const navBarOpen = document.querySelector('#navbar-h');
 navBarOpen.addEventListener('click', () => {
   const navbarOptions = document.querySelector('#navbar-options');
   navbarOptions.classList.replace('nav-right-text', 'nav-right-text-open');
+
+  const closeIcon = document.querySelector('#close-icon-c');
+  closeIcon.classList.replace('hidden', 'close-icon-visible');
 });
 
 const navBarClose = document.querySelector('#navbar-c');
 navBarClose.addEventListener('click', () => {
   const navbarOptions = document.querySelector('#navbar-options');
   navbarOptions.classList.replace('nav-right-text-open', 'nav-right-text');
+
+  const closeIcon = document.querySelector('#close-icon-c');
+  closeIcon.classList.replace('close-icon-visible', 'hidden');
 });
 
 const sectionWorkClose = document.querySelector('#work-c');
