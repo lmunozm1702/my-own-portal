baedea8ae978fa109c1f44eb8da7c7704d15e643
Luis Muñoz <lmunozm1702@gmail.com>
2022-10-13 18:23:00 -0300

    Solve linter errors

--- assets/js/index_contact.js
+++ assets/js/index_contact.js
@@ -1,36 +1,35 @@
 const form = document.querySelector('#contactme');
 
 function validateEmail(email) {
-    if (email.value.trim() === email.value.trim().toLowerCase()) {
-        return true;
-    }
-    return false;
+  if (email.value.trim() === email.value.trim().toLowerCase()) {
+    return true;
+  }
+  return false;
 }
 
 function setMessage(id, message, type) {
-    const divId = document.querySelector(`#${id}`);
+  const divId = document.querySelector(`#${id}`);
 
-    const errorDiv = document.createElement('div');
-    errorDiv.id = `${id}-error-message`;
-    errorDiv.className = `contact-${type}`;
-    errorDiv.textContent = `${message}`;
+  const errorDiv = document.createElement('div');
+  errorDiv.id = `${id}-error-message`;
+  errorDiv.className = `contact-${type}`;
+  errorDiv.textContent = `${message}`;
 
-    form.insertBefore(errorDiv, divId);
+  form.insertBefore(errorDiv, divId);
 }
 
-form.addEventListener('submit', function (event) {
-    event.preventDefault();
+form.addEventListener('submit', event => {
+  event.preventDefault();
 
-    let errorDiv = document.querySelector('#contact-email-error-message');
+  const errorDiv = document.querySelector('#contact-email-error-message');
 
-    if (errorDiv) {
-        errorDiv.remove();
-    }
+  if (errorDiv) {
+    errorDiv.remove();
+  }
 
-    if (!validateEmail(form.elements['contact-email'])) {
-        setMessage('contact-email', 'Ooops!, please write your email in lowercase!', 'error');
-    }
-    else {
-        form.submit();
-    };
-});
+  if (!validateEmail(form.elements['contact-email'])) {
+    setMessage('contact-email', 'Ooops!, please write your email in lowercase!', 'error');
+  } else {
+    form.submit();
+  };
+})
